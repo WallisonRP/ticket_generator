b5cc84348b1f9371dc5636f093ba0086e7d7fa9f
WallisonRP <wallison0076@gmail.com>
2023-03-24 20:34:57 -0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:ticket_generator/intro.dart';
 import 'package:ticket_generator/shop_page.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MaterialApp(
     home: ShopPage()
   ));
