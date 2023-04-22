ed4dc59d1eca0ecedb650800d6bb857db7711293
WallisonRP <wallison0076@gmail.com>
2023-04-22 20:52:57 -0300

    numero de senhas

--- lib/teste.dart
+++ lib/teste.dart
@@ -11,16 +11,25 @@ class Teste extends StatefulWidget {
 
 class _TesteState extends State<Teste> {
   var db = FirebaseFirestore.instance;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController cellController = TextEditingController();
 
-  _teste() async {
-    // var statsRef = db.collection('stories').doc('--stats--');
-    // var storyRef = db.collection('stories').doc(DateTime.now().microsecondsSinceEpoch.toString());
-    await FirebaseFirestore.instance.collection('stories').doc('--stats--').update({"storyCount": FieldValue.increment(1)});
+  _generateTicket() async {
+    var statsRef = db.collection('senhasRetiradas').doc('--quantidadeSenhas--');
+    var storyRef = db
+        .collection('senhasRetiradas')
+        .doc(DateTime.now().microsecondsSinceEpoch.toString());
+    // await FirebaseFirestore.instance.collection('senhasRetiradas').doc('--quantidadeSenhas--').update({"contadorSenhas": FieldValue.increment(1)});
 
-    // var batch = db.batch();
-    // batch.set(storyRef, { 'title': 'New Story!' });
-    // batch.set(statsRef, { 'storyCount': FieldValue.increment(1) });
-  
+    var batch = db.batch();
+    var docSnapshot = await db.collection('senhasRetiradas').get();
+    print(docSnapshot.size);
+    batch.set(
+        storyRef, {'name': nameController.text, 'cell': cellController.text, 'senha': docSnapshot.size});
+    batch.set(statsRef, {'contadorSenhas': FieldValue.increment(1)},
+        SetOptions(merge: true));
+    batch.commit();
+
   }
 
   @override
@@ -30,9 +39,25 @@ class _TesteState extends State<Teste> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () {
-              _teste();
-            }, child: Text("Teste"))
+            TextField(
+              decoration: InputDecoration(labelText: 'Nome'),
+              controller: nameController,
+              onChanged: (value) {
+                print(nameController.text);
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Celular'),
+              controller: cellController,
+              onChanged: (value) {
+                print(cellController.text);
+              },
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _generateTicket();
+                },
+                child: Text("Teste"))
           ],
         ),
       ),
